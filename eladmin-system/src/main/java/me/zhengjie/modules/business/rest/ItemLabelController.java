b4efc5cc692a8e635353da414df58a486a865ac0
wangpf2 <wangpf2@ziroom.com>
2025-05-27 19:59:04 +0800

    test my mac mini m4

--- eladmin-system/src/main/java/me/zhengjie/modules/business/rest/ItemLabelController.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/business/rest/ItemLabelController.java
@@ -41,7 +41,7 @@ public class ItemLabelController {
     @Autowired
     private ItemLabelService itemLabelService;
 
-    @ApiOperation("创建/更新产品标签")
+    @ApiOperation("创建/更新产品标签1")
     @AnonymousPostMapping(value = "/v1/createOrUpdateItemLabel")
     public ResponseEntity<CreateItemLabelResponse> createOrUpdateItemLabel(@RequestBody CreateItemLabelRequest request) throws Exception {
         Integer labelLevel = request.getLabelLevel();
